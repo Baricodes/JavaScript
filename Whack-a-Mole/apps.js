e5dbdba67788c5f1f75a04f688fcf8a3a1bd7fe2
jhenry3 <jhenry3@uno.edu>
2021-10-25 17:46:45 -0500

    Added comments

--- Whack-a-Mole/apps.js
+++ Whack-a-Mole/apps.js
@@ -5,24 +5,33 @@ const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
 let score = document.querySelector('#score')
 
+// Players score
 let result = 0
+// Game clock
 let curretTime = timeLeft.textContent
  
+// Function for randomly placing the mole around in the grid
 function randomSquare() {
+    // Using for each function to clear the board
     square.forEach(className => {
         className.classList.remove('mole')
     })
-
+    // Using to select a random position on the grid
     let randomPosition = square[Math.floor(Math.random() * 9)]
+    // Adding the mole to the square
     randomPosition.classList.add('mole')
 
 
-    // assign the id of the radnomPosition to hitPosition for us to use later
+    // Assign the id of the radnomPosition to hitPosition for us to use later
     hitPosition = randomPosition.id
 }
 
+// A for each function to set up the scoring system
 square.forEach(id => {
+    // Adding an event listner to each square
     id.addEventListener('mouseup', () => {
+        // If current square is equal to hit position
+        // Add 1 to the score and update the score
         if (id.id === hitPosition) {
             result = result + 1
             score.textContent = result
@@ -30,11 +39,15 @@ square.forEach(id => {
     })
 })
 
+// Function for moving the mole around the grid
 function moveMole() {
     let timerId = null 
-    timerId = setInterval(randomSquare, 1000)
+    // Setting a time interval up for delayed calls to randomSquare()
+    // which places the mole around the grid
+    timerId = setInterval(randomSquare, 800)
 }
 
+// Function for decrementing game clock
 function countDown() {
     curretTime--
     timeLeft.textContent = curretTime
@@ -44,7 +57,7 @@ function countDown() {
         alert('GAME OVER! Your final score is ' + result)
     }
 }
-
+// Interval for game clock count down
 let timerId = setInterval(countDown, 1000)
 
 moveMole()
