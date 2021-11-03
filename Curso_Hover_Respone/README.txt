b4cc936d64f6fbc909adae39b99d74e9f6b73c37
jhenry3 <jhenry3@uno.edu>
2021-11-02 23:33:32 -0500

    Adding README

---
+++ Curso_Hover_Respone/README.txt
@@ -0,0 +1,13 @@
+How it works:
+
+Using an event listener the div element listens for the presence
+of the mouse hovering over it and add a certain class .hover
+in from the styles.css file in repsonse. The oppisite happens
+when the absence of the mouse if detected through and event 
+listener, i.e the .hover class is removed.
+
+What I learned:
+
+Using arrow functions, eventListeners. 
+Adding and removing classes from HTML 
+elements.  
--- Curso_Hover_Respone/app.js
+++ Curso_Hover_Respone/app.js
@@ -1,12 +1,18 @@
 const circle = document.querySelector('#circle')
 
+// Adding an eventlist to listen for presence
+// of the mouse over the div element
 circle.addEventListener('mouseenter', () => {
+    // Adding hover class to div element
     if(!circle.classList.contains('hover')) {
         circle.classList.add('hover');
     }
 });
 
+// Reverse logic of above. Listening for the
+// absence of the mouse's presence
 circle.addEventListener('mouseleave', () => {
+    // Removing hover class from div element
     if(circle.classList.contains('hover')){
         circle.classList.remove('hover');
     }
